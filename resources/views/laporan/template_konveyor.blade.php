99bfa1e34c193b801daffd2c6e448e11e3438e80
Tegar Ihsan <ihsanrafid30@gmail.com>
2026-01-20 01:49:50 +0700

    TEGAR: add conveyor inspection report template for konveyor.

--- resources/views/laporan/template_konveyor.blade.php
+++ resources/views/laporan/template_konveyor.blade.php
@@ -22,6 +22,10 @@
         </div>
     </footer>
 
+
+
+
+
     {{-- ================= BAB 1 ================= --}}
     <div class="bab1_container">
         <h3 style="text-align:center; margin-top: 0px;">
@@ -64,7 +68,485 @@
             guna menjalankan amanat undang-undang serta peraturan yang telah ditetapkan serta mewujudkan rasa aman dan
             nyaman ketika berada di tempat kerja.
         </p>
+        <h4 class="sub_bab1 latbel">1.2 Identitas Pesawat Angkat dan Angkut</h4>
+        <p>
+            Identitas dari pesawat angkat dan angkut jenis Belt Conveyor yang kami lakukan pemeriksaan dan pengujian,
+            sebagai berikut:
+        </p>
+        <table>
+            <tr>
+                <td>Jenis Pesawat</td>
+                <td>: Konveyor</td>
+            </tr>
+            <tr>
+                <td>Merk / Model</td>
+                <td>: Feihong/-</td>
+            </tr>
+            <tr>
+                <td>No. Unit</td>
+                <td>: 005</td>
+            </tr>
+            <tr>
+                <td>Nama pabrik pembuatan</td>
+                <td>: PT Feihong Trading International</td>
+            </tr>
+            <tr>
+                <td>Tahun Pembuatan</td>
+                <td>: 2021</td>
+            </tr>
+            <tr>
+                <td>Kapasitas</td>
+                <td>: 50 Kg</td>
+            </tr>
+            <tr>
+                <td>Pemilik pesawat</td>
+                <td>: PT STAR ALLIANCE INTIMATES</td>
+            </tr>
+        </table>
+        <h4 class="sub_bab1 latbel">1.3 Fungsi Pesawat</h4>
+        <p>
+            Jenis pekerjaan yang dilakukan dengan menggunakan pesawat angkut jenis konveyor adalah memindahkan barang
+            dari
+            satu ke tempat yang lain
+        </p>
+        <h4 class="sub_bab1 latbel">1.4 Dasar Hukum/Refrensi</h4>
+        <p>
+            Dasar hukum yang kami gunakan dalam melakukan kegiatan pemeriksaan dan pengujian terhadap pesawat angkat dan
+            angkut adalah sebagai berikut:
+        </p>
+        <ol>
+            <li>Undang-Undang Nomor 1 Tahun 1970 tentang Keselamatan Kerja</li>
+            <li>Undang-Undang Nomor 13 Tahun 2003 tentang Ketenagakerjaan</li>
+            <li>Peraturan Pemerintah No.50 Tahun 2012 tentang Penerapan Sistem Manajemen Keselamatan dan Kesehatan Kerja
+            </li>
+            <li>Peraturan Menteri Tenaga Kerja RI No. 04/MEN/1995 tentang Jasa Keselamatan dan Kesehatan Kerja</li>
+            <li>Peraturan Menteri Tenaga Kerja RI No. 08/MEN/2020 tentang Pesawat Angkat dan Pesawat Angkut</li>
+        </ol>
+        <p>Refrensi dan standar lain yang digunakan dalam pelaksanaan pemeriksaan dan pengujian jenis konveyor adalah :
+        </p>
+        <ol>
+            <li>Manual book pesawat angkut</li>
+            <li>ASME (American Society of Mechnanical Engineers)</li>
+        </ol>
+        <h4 class="sub_bab1 latbel">1.5 Alat Uji dan Alat Ukur</h4>
+        <p>
+            Alat uji dan alat ukur yang digunakan pada pelaksanaan pemeriksaan dan pengujian pesawat angkut jenis
+            konveyor,
+            antara lain :
+        </p>
+        <ol>
+            <li>Meteran gulung</li>
+            <li>Jangka sorong</li>
+            <li>Stop Watch</li>
+        </ol>
     </div>
+    <div style="page-break-after: always;"></div>
+
+
+
+
+
+    {{-- ================= BAB 2 ================= --}}
+    <div class="bab1_container">
+        <h3 style="text-align:center; margin-top: 0px;">
+            BAB II<br>PEMERIKSAAN DAN PENGUJIAN
+        </h3>
+        <h4 class="sub_bab1 latbel">2.1 Pemeriksaan Dokumen</h4>
+        <p>
+            Pemeriksaan dokumen meliputi pemeriksaan dokumen negara dan dokumen pesawat angkat dan pesawat angkut.
+        </p>
+        <P>
+            Dokumen negara terdiri dari:
+        </P>
+        <ol>
+            <li>Surat Keterangan (pemeriksaan sebelumnya)</li>
+            <li>Lisensi Operator</li>
+            <li>Stiker memenuhi persyaratan K3</li>
+        </ol>
+        <p>
+            Dokumen pesawat angkat dan angkut terdiri dari:
+        </p>
+        <ol>
+            <li><i>Manual book</i></li>
+            <li>Standar Operasional Prosedur (SOP) pengoperasian pesawat angkat dan angkut</li>
+        </ol>
+        <h4 class="sub_bab1 latbel">2.1 Pemeriksaan Dokumen</h4>
+        <p>
+            Pemeriksaan dokumen meliputi pemeriksaan dokumen negara dan dokumen pesawat angkat dan pesawat angkut.
+        </p>
+        <h4 class="sub_bab1 latbel">2.2 Pemeriksaan Visual</h4>
+        <p>
+            Pemeriksaan visual dilakukan untuk mengetahui kondisi fisik secara keseluruhan mesin sesuai dengan regulasi
+            keselamatan dan kesehatan di tempat kerja yang ada, termasuk di dalamnya perlengkapan <i>safety device</i>,
+            dan
+            <i>safety guarding</i>.
+        </p>
+        <h4 class="sub_bab1 latbel">2.3 Pengukuran Dimensi</h4>
+        <p>
+            Pengukuran dimensi dilakukan untuk mencocokan ukuran yang tertulis di dalam <i>manual book</i> dengan yang
+            terdapat
+            di lapangan.
+        </p>
+        <h4 class="sub_bab1 latbel">2.4 Pengujian Tidak Merusak</h4>
+        <p>
+            Pengujian ini dilakukan untuk mengetahui apakah terjadi kecacatan pada daerah-daerah kritis yang menerima
+            beban. Pengujian tidak merusak yang kami lakukan menggunakan metode pengujian <i>dye penetrant test</i>.
+        </p>
+        <h4 class="sub_bab1 latbel">2.5 Pengujian Fungsi</h4>
+        <p>
+            Pengujian fungsi dilakukan untuk mengetahui bahwa komponen-komponen yang terpasang pada pesawat angkat dan
+            angkut ini berfungsi dengan baik. Komponen yang dilakukan uji fungsi di antaranya: kelistrikan, fungsi
+            <i>safety device</i>, fungsi motor penggerak.
+        </p>
+        <h4 class="sub_bab1 latbel">2.6 Pengujian Beban Dinamis</h4>
+        <p>
+            Pengujian beban dinamis dilakukan untuk mengetahui bahwa pesawat dapat bekerja dengan baik pada saat
+            mengangkat beban.
+        </p>
+        <h4 class="sub_bab1 latbel">2.7 Pengujian Beban Statis</h4>
+        <p>
+            Pengujian beban statis dilakukan untuk mengetahui kemampuan pesawat dalam mengangkat beban melebihi safe
+            working load yang ada. Beban yang diangkat adalah 110%-125%
+        </p>
+
+    </div>
+    <div style="page-break-after: always;"></div>
+
+
+    {{-- ================= BAB 3 ================= --}}
+    <div class="bab1_container">
+        <h3 style="text-align:center; margin-top: 0px;">
+            BAB III<br>PEMERIKSAAN DAN PENGUJIAN
+        </h3>
+        <table>
+            <tr>
+                <td colspan="2">Identitas Pemeriksa</td>
+            </tr>
+            <tr>
+                <td colspan="2">Yang Memeriksa</td>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td>: Naufal Arya Prihandiko</td>
+            </tr>
+            <tr>
+                <td>No.Kep</td>
+                <td>: 5/1179/AS.01.04./VII/2023</td>
+            </tr>
+            <tr>
+                <td>Bidang</td>
+                <td>: Pesawat Angkat dan Angkut</td>
+            </tr>
+            <tr>
+                <td>Pelaksanaan</td>
+                <td>: 22 April 2024</td>
+            </tr>
+        </table>
+        <h4 class="sub_bab1 latbel">3.1 Check List Pemeriksaan</h4>
+
+        <!-- Data Umum -->
+        <p>I. Data Umum</p>
+        <table border="1" width="100%" cellspacing="0" cellpadding="6">
+            <tr>
+                <td colspan="4">LAPORAN PEMERIKSAAN DAN PENGUJIAN
+                    (PERTAMA/BERKALA/KHUSUS/ULANG)
+                    KONVEYOR <br> NO : 010 /CTS/RIKSAUJI-PAA/IV/2024</td>
+            </tr>
+            <tr>
+                <td colspan="4">I. DATA UMUM</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Pemilik</td>
+                <td>:</td>
+                <td>PT Star Alliance Intimates</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Alamat</td>
+                <td>:</td>
+                <td>Jalan Taman Industri BSB Blok D-1, Kec. Mijen, Kota Semarang</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Pemakai</td>
+                <td>:</td>
+                <td>PT Star Alliance Intimates</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Pengurus / Sub Kontraktor / Penanggung Jawab</td>
+                <td>:</td>
+                <td>Bpk. Suyut</td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>Lokasi Unit</td>
+                <td>:</td>
+                <td>Area Produksi</td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td>Jenis Pesawat</td>
+                <td>:</td>
+                <td>Belt Conveyor</td>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td>Pabrik Pembuat</td>
+                <td>:</td>
+                <td>PT Feihong Trading International</td>
+            </tr>
+            <tr>
+                <td>8</td>
+                <td>Merek / Type</td>
+                <td>:</td>
+                <td>Feihong / -</td>
+            </tr>
+            <tr>
+                <td>9</td>
+                <td>Lokasi dan Tahun Pembuatan</td>
+                <td>:</td>
+                <td>China, 2021</td>
+            </tr>
+            <tr>
+                <td>10</td>
+                <td>No. Serie / No. Unit</td>
+                <td>:</td>
+                <td>Unit 005</td>
+            </tr>
+            <tr>
+                <td>11</td>
+                <td>Kapasitas / Bobot Kerja</td>
+                <td>:</td>
+                <td>50 Kg</td>
+            </tr>
+            <tr>
+                <td>12</td>
+                <td>Standard Yang Dipakai</td>
+                <td>:</td>
+                <td>Permenaker No. 8 Tahun 2020</td>
+            </tr>
+            <tr>
+                <td>13</td>
+                <td>Digunakan Untuk</td>
+                <td>:</td>
+                <td>Memindahkan Barang Produksi (Garmen)</td>
+            </tr>
+            <tr>
+                <td>14</td>
+                <td>Nomor Surat Keterangan</td>
+                <td>:</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <td>15</td>
+                <td>No. Lisensi K3 Operator / Masa Berlaku s/d</td>
+                <td>:</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <td>16</td>
+                <td>Data Riwayat Pesawat</td>
+                <td>:</td>
+                <td>Pemeriksaan Berkala</td>
+            </tr>
+        </table>
+
+        <!-- Data Teknis -->
+        <p>II. Data teknis</p>
+        <table border="1" width="100%" cellspacing="0" cellpadding="6">
+            <tr>
+                <td colspan="4">II. DATA TEKNIK</td>
+            </tr>
+            <tr>
+                <td rowspan="7">Spesifikasi
+                    Konveyor</td>
+                <td>Jenis</td>
+                <td>Belt Conveyor</td>
+            </tr>
+            <tr>
+                <td>Tahun Pembuatan</td>
+                <td>2021</td>
+            </tr>
+            <tr>
+                <td>Kapasitas/ Bobot Kerja</td>
+                <td>50 Kg</td>
+            </tr>
+            <tr>
+                <td>Panjang Keseluruhan</td>
+                <td>12 Meter</td>
+            </tr>
+            <tr>
+                <td>Tinggi Keseluruhan</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <td>Lebar</td>
+                <td>0,64 Meter</td>
+            </tr>
+            <tr>
+                <td>Ban berjalan terbuat dari bahan</td>
+                <td>Rubber PVC</td>
+            </tr>
+            <tr>
+                <td rowspan="9">Motor Penggerak (1)</td>
+                <td>Jenis Penggerak</td>
+                <td>Motor Elektrik</td>
+            </tr>
+            <tr>
+                <td>Merk</td>
+                <td>Feihong</td>
+            </tr>
+            <tr>
+                <td>Negara pembuat</td>
+                <td>China</td>
+            </tr>
+            <tr>
+                <td>Model</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <td>No. Seri/Unit</td>
+                <td>Unit 003</td>
+            </tr>
+            <tr>
+                <td>Tegangan</td>
+                <td>220 V</td>
+            </tr>
+            <tr>
+                <td>Daya</td>
+                <td>2,2 kW</td>
+            </tr>
+            <tr>
+                <td>Merek / tahun pembuatan</td>
+                <td>Feihong/2021</td>
+            </tr>
+            <tr>
+                <td>Pabrik pembuat</td>
+                <td>PT Feihong Trading International</td>
+            </tr>
+        </table>
+
+
+
+        <!--  III. Pemeriksaan Visual dan Fungsi -->
+        <!-- 1. Pemeriksaan dengan Mesin Mati -->
+        <p>III. Pemeriksaan Visual dan Fungsi</p>
+        <p>1. Pemeriksaan dengan Mesin Mati</p>
+
+        <!-- 2. Pemeriksaan dengan Mesin Hidup  -->
+        <p>2. Pemeriksaan dengan Mesin Hidup</p>
+
+        <!-- IV. Pemeriksaan Tidak Merusak -->
+        <p>IV. Pemeriksaan Tidak Merusak</p>
+        <p>Terhadap Konstruksi dan Komponen</p>
+
+
+        <!-- V. Pemeriksaan Tidak Merusak -->
+        <p>V. Pengujian Dinamis</p>
+
+        <!-- VI. Pengujian Statis  -->
+        <p>VI. Pengujian Statis</p>
+    </div>
+    <div style="page-break-after: always;"></div>
+
+
+
+
+    {{-- ================= BAB IV ================= --}}
+    <div class="bab1_container">
+        <h3 style="text-align:center; margin-top: 0px;">
+            BAB IV<br>PENUTUP
+        </h3>
+
+        <!-- Kesimpulan -->
+        <h4 class="sub_bab1 latbel">4.1 Kesimpulan</h4>
+        <p>
+            Hasil pemeriksaan dan pengujian pada unit <b>Konveyor Feihong Unit 005</b> bertempat di <b>PT Star Alliance
+                Intimates</b>,
+            Jalan Taman Industri BSB Blok D-1, Kec. Mijen, Kota Semarang, didapatkan hasil sebagai berikut:
+        </p>
+        <ol>
+            <li>Pengukuran dimensi pada unit konveyor hasil baik</li>
+            <li>Pemeriksaan motor penggerak dalam kondisi baik</li>
+            <li>Pemeriksaan visual pada belt conveyor dalam kondisi baik</li>
+            <li>Pengujian fungsi pada unit konveyor dalam kondisi baik</li>
+            <li>Pengujian tidak merusak (secara visual) dalam kondisi baik, tidak ditemukan adanya crack atau retak pada
+                unit</li>
+            <li>Pengujian beban pada saat pelaksanaan riksa uji dengan beban 10 Kg (beban maksimal operasi sehari-hari)
+                hasil baik</li>
+        </ol>
+        <p>
+            Dari hasil pemeriksaan dan pengujian di atas maka dapat disimpulkan bahwa unit <b>Konveyor Feihong Unit
+                005</b>
+            dalam kondisi baik dan <b>Memenuhi/Tidak Memenuhi</b> syarat Keselamatan dan Kesehatan Kerja serta layak
+            untuk
+            dioperasikan dengan memenuhi rekomendasi di bawah ini.
+        </p>
+
+        <!-- saran saran -->
+        <h4 class="sub_bab1 latbel">4.2 Saran-saran</h4>
+        <ol>
+            <li>Dalam pengoperasiannya tidak boleh melebihi SWL yang telah ditentukan (25 Kg)</li>
+            <li>Lengkapi unit dengan sign kapasitas beban yang boleh diangkut</li>
+            <li>Lengkapi unit dengan SOP (Standar Operasional Pengoperasian) dan pasang pada tempat yang mudah dilihat
+                (di dekat panel pengoperasian)</li>
+            <li>Dalam pengoperasiannya harus berpedoman pada SOP (Standar Operasional Prosedur).</li>
+            <li>Konveyor harus dioperasikan oleh operator yang memiliki lisensi K3 Operator dari Kemnaker RI</li>
+            <li>Lengkapi unit dengan emergency stop</li>
+            <li>Lakukan pelumasan pada rantai transmisi konveyor secara rutin</li>
+            <li>Lakukan daily check secara rutin sebelum dan sesudah pengoperasian</li>
+            <li>Pastikan lantai kerja di samping konveyor selalu dalam keadaan kering dan bersih dari sampah</li>
+            <li>Pada lantai kerja konveyor sebaiknya diberi demarkasi guna memberikan tanda daerah aman</li>
+            <li>Harap persiapkan Pemeriksaan dan Pengujian berkala selambat-lambatnya 22 April 2025</li>
+        </ol>
+        <h4 class="sub_bab1 latbel">4.3 Penutup</h4>
+        <p>
+            Dmeikian Laporan Hasil Pemeriksaan dan Pengujian (LHPP) ini kami susun agar selanjutnya dapat digunakan
+            sebagai landasan/acuan dalam penerbitan Surat Keterangan Layak K3 dari Dinas Tenaga Kerja dan Transmigrasi
+            Provinsi Jawa Tengah.
+        </p>
+
+        <!-- TTD -->
+        <div class="ttd_container_bab3">
+            <div class="ttd_bab3">
+                <p>
+                    Semarang, 23 April 2024
+                    <br>
+                    PT CAKRA TEKNIKA SOLUSI<br>
+                    Yang Memeriksa dan Menguji<br>
+                    <strong>
+                        Ahli K3 Bidang<br>
+                        Pesawat Angkat dan Pesawat Angkut
+                    </strong>
+                    <b>PT CAKRA TEKNIKA SOLUSI</b>
+                </p>
+
+                <p>
+                    <strong style="text-transform: capitalize; text-decoration: underline;">
+                        NAUFAL ARYA PRIHANDIKO
+                    </strong><br>
+                    <strong style="text-decoration: underline;">
+                        No. SKP. <br>
+                        SK : 5/1179/AS.01.04/VII/2023
+                    </strong>
+                </p>
+            </div>
+            <div style="page-break-after: always;"></div>
+
+
+
+
+            {{-- ================= BAB IV ================= --}}
+            <div class="bab1_container">
+                <h3 style="text-align:center; margin-top: 0px;">
+                    DOKUMENTASI
+                </h3>
+            </div>
+        </div>
+
+
 </body>
 
 </html>
