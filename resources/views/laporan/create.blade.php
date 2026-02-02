8c4565e3f3e9c6426359f826c1be86ceb0d4409c
AlterftHiatus <acenkyudi12@gmail.com>
2026-02-02 15:58:15 +0700

    tes

--- resources/views/laporan/create.blade.php
+++ resources/views/laporan/create.blade.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
-    <link rel="stylesheet" href="{{ asset('css/create.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/create.css') }}?v={{ time() }}">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 </head>
 
@@ -381,7 +381,7 @@
         </div>
     </div>
 
-    <script src="{{ asset('js/create.js') }}" defer></script>
+    <script src="{{ asset('js/create.js') }}?v={{ time() }}"></script>
 </body>
 
 </html>
