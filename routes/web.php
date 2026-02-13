40ee3f1165a83c83e25d5d9d72658f3cf288f08a
AlterftHiatus <acenkyudi12@gmail.com>
2026-02-14 02:00:03 +0700

    add fitur auth

--- routes/web.php
+++ routes/web.php
@@ -5,53 +5,116 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LaporanController;
 use App\Http\Controllers\KonveyorController;
 use App\Http\Controllers\PenggerakMulaController;
+use App\Http\Controllers\ProfileController;
 
-// Route::get('/tes', function () {
-//     return view('laporan.pdf');
-// });
+/*
+|--------------------------------------------------------------------------
+| PUBLIC ROUTES
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/', [HomeController::class, 'index']);
-Route::get('/dashboard', [LaporanController::class, 'index'])->name('dashboard');
-Route::post('/laporan', [LaporanController::class, 'store']);
-Route::delete('/laporan/{id}', [LaporanController::class, 'destroy'])->name('laporan.destroy');
-Route::get('/laporan/create', [LaporanController::class, 'create']);
-Route::get('/laporan/{id}/pdf', [LaporanController::class, 'generatePdf']);
+Route::view('/profile_perusahaan', 'profile-perusahaan');
+Route::view('/tentang', 'tentang');
+Route::view('/contact', 'contact-us');
 
-// form edit laporan
-Route::get('/laporan/{id}/edit', [LaporanController::class, 'edit'])->name('laporan.edit');
+/*
+|--------------------------------------------------------------------------
+| PROTECTED ROUTES (WAJIB LOGIN)
+|--------------------------------------------------------------------------
+*/
 
-// update laporan
-Route::put('/laporan/{id}/update', [LaporanController::class, 'update'])->name('laporan.update');
+Route::middleware(['auth'])->group(function () {
 
-Route::get('/laporan/template_konveyor', [KonveyorController::class, 'downloadPdf']);
-Route::get('/laporan/konveyor/create', [KonveyorController::class, 'create'])->name('laporan.konveyor.create');
-Route::get('/laporan/penggerak-mula/create',[PenggerakMulaController::class, 'create'])->name('laporan.penggerak-mula.create');
-Route::post('/laporan/penggerak-mula',[PenggerakMulaController::class, 'store'])->name('laporan.penggerak-mula.store');
-Route::post('/laporan/konveyor', [KonveyorController::class, 'store'])->name('laporan.konveyor.store');
+    /*
+    |--------------------------------------------------------------------------
+    | DASHBOARD
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/dashboard', [LaporanController::class, 'index'])
+        ->name('dashboard');
 
 
-// ini front end  
-Route::get('/input_laporan', function () {
-    return view('input-laporan');
+    /*
+    |--------------------------------------------------------------------------
+    | CONCRETE MIXER
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/laporan/create', [LaporanController::class, 'create']);
+    Route::post('/laporan', [LaporanController::class, 'store']);
+
+    Route::get('/laporan/{id}/edit', [LaporanController::class, 'edit']);
+    Route::put('/laporan/{id}/update', [LaporanController::class, 'update']);
+
+    Route::delete('/laporan/{id}', [LaporanController::class, 'destroy']);
+    Route::get('/laporan/{id}/pdf', [LaporanController::class, 'generatePdf']);
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | KONVEYOR
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/laporan/template_konveyor', [KonveyorController::class, 'downloadPdf']);
+
+    Route::get('/laporan/konveyor/create', [KonveyorController::class, 'create'])
+        ->name('laporan.konveyor.create');
+
+    Route::post('/laporan/konveyor', [KonveyorController::class, 'store'])
+        ->name('laporan.konveyor.store');
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | PENGGERAK MULA
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get(
+        '/laporan/penggerak-mula/create',
+        [PenggerakMulaController::class, 'create']
+    )->name('laporan.penggerak-mula.create');
+
+    Route::post(
+        '/laporan/penggerak-mula',
+        [PenggerakMulaController::class, 'store']
+    )->name('laporan.penggerak-mula.store');
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | HALAMAN STATIC INTERNAL
+    |--------------------------------------------------------------------------
+    */
+
+    Route::view('/input_laporan', 'input-laporan');
+    Route::view('/dunlut', 'dunlut');
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | PROFILE (BREEZE)
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
 });
 
-Route::get('/profile_perusahaan', function () {
-    return view('profile-perusahaan');
-});
 
-Route::get('/riwayat', function () {
-    return view('riwayat');
-});
+/*
+|--------------------------------------------------------------------------
+| AUTH ROUTES (LOGIN, LOGOUT, REGISTER)
+|--------------------------------------------------------------------------
+*/
 
-Route::get('/tentang', function () {
-    return view('tentang');
-});
-
-Route::get('/contact', function () {
-    return view('contact-us');
-});
-
-// Ini buat download pdf
-Route::get('/dunlut', function () {
-    return view('dunlut');
-});
+require __DIR__ . '/auth.php';
